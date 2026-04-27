ae734115bfa9966c60d2198a60e3f6811e5ff865
abenezer@csa <abenezerhaymanot@gmail.com>
2026-04-27 20:35:11 +0300

    (test) new test swites

---
+++ tests/subaccounts_test.rs
@@ -0,0 +1,51 @@
+use chapa_rust::{
+    client::ChapaClient,
+    config::ChapaConfigBuilder,
+    models::payment::{CreateSubaccountOptions, SplitType},
+};
+use mockito::{Matcher, Server};
+use serde_json::json;
+
+#[tokio::test]
+async fn test_create_subaccount() {
+    let mut server = Server::new_async().await;
+
+    let mock = server
+        .mock("POST", "/v1/subaccount")
+        .match_header("authorization", Matcher::Regex(r"^Bearer .+$".to_string()))
+        .with_status(200)
+        .with_body(
+            serde_json::to_string(&json!({
+                "status": "success",
+                "message": "Subaccount created",
+                "data": {
+                     "id": "837b4e5e-57c8-4e39-b2df-66e7886b8bdb"
+               }
+            }))
+            .unwrap(),
+        )
+        .create_async()
+        .await;
+
+    let config = ChapaConfigBuilder::new()
+        .base_url(server.url())
+        .api_key("test-key")
+        .build()
+        .unwrap();
+    let client = ChapaClient::from_config(config).unwrap();
+
+    let subaccount = CreateSubaccountOptions {
+        account_name: "Abebe Bikila ".to_string(),
+        bank_code: 128,
+        account_number: "0123456789".to_string(),
+        split_type: Some(SplitType::PERCENTAGE),
+        split_value: Some(0.2),
+    };
+    let response = client.create_subaccount(subaccount).await.unwrap();
+
+    assert_eq!(response.status.as_str(), "success");
+    assert!(!response.message.is_null());
+    assert_eq!(response.data.id, "837b4e5e-57c8-4e39-b2df-66e7886b8bdb");
+
+    mock.assert_async().await;
+}
---
+++ tests/transfer_test.rs
@@ -0,0 +1,168 @@
+use chapa_rust::{client::ChapaClient, config::ChapaConfigBuilder};
+use mockito::{self, Matcher};
+
+#[tokio::test]
+async fn test_get_all_transfers_no_filters() {
+    let mut server = mockito::Server::new_async().await;
+
+    let mock = server
+        .mock("GET", "/v1/transfers")
+        .match_header(
+            "authorization",
+            Matcher::Regex(r#"^Bearer .+$"#.to_string()),
+        )
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(
+            serde_json::to_string(&serde_json::json!({
+                "message": "Transfer details fetched",
+                "status": "success",
+                "pagination": {
+                    "current_page": 1,
+                    "first_page_url": "https://api.chapa.co/v1/transfers?page=1",
+                    "last_page": 16,
+                    "last_page_url": "https://api.chapa.co/v1/transfers?page=16",
+                    "next_page_url": "https://api.chapa.co/v1/transfers?page=2",
+                    "path": "https://api.chapa.co/v1/transfers?page=1",
+                    "per_page": 10,
+                    "prev_page_url": null,
+                    "to": 10,
+                    "total": 159,
+                    "error": []
+                },
+                "data": [
+                    {
+                        "account_name": "suz",
+                        "account_number": "1",
+                        "currency": "ETB",
+                        "amount": 1,
+                        "charge": 0,
+                        "transfer_type": "bank",
+                        "chapa_reference": "7039636674388",
+                        "bank_code": 656,
+                        "bank_name": "Awash Bank",
+                        "bank_reference": null,
+                        "status": "failed/cancelled",
+                        "reference": "7039636674388",
+                        "created_at": "2022-10-24T14:48:06.000000Z",
+                        "updated_at": "2023-10-10T09:20:27.000000Z"
+                    }
+                ]
+            }))
+            .unwrap(),
+        )
+        .create_async()
+        .await;
+
+    let config = ChapaConfigBuilder::new()
+        .base_url(server.url())
+        .api_key("CHASECK-xxxxxxxxxxxxxxxx")
+        .build()
+        .unwrap();
+    let client = ChapaClient::from_config(config).unwrap();
+
+    let response = client
+        .get_all_transfers(None, None, None, None)
+        .await
+        .unwrap();
+    assert_eq!(response.status, "success");
+    assert!(response.message.is_null());
+    assert!(response.data.is_none());
+    mock.assert_async().await;
+}
+
+#[tokio::test]
+async fn test_get_all_transfers_with_filters() {
+    let mut server = mockito::Server::new_async().await;
+
+    let mock = server
+        .mock(
+            "GET",
+            "/v1/transfers?from_date=2025-01-01&to_date=2025-01-31&currency=ETB&status=success",
+        )
+        .match_header(
+            "authorization",
+            Matcher::Regex(r#"^Bearer .+$"#.to_string()),
+        )
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(
+            serde_json::to_string(&serde_json::json!({
+                "message": "Transfer details fetched",
+                "status": "success",
+                "meta": {
+                    "current_page": 1,
+                    "first_page_url": "https://api.chapa.co/v1/transfers?page=1",
+                    "last_page": 16,
+                    "last_page_url": "https://api.chapa.co/v1/transfers?page=16",
+                    "next_page_url": "https://api.chapa.co/v1/transfers?page=2",
+                    "path": "https://api.chapa.co/v1/transfers?page=1",
+                    "per_page": 10,
+                    "prev_page_url": null,
+                    "to": 10,
+                    "total": 159,
+                    "error": []
+                },
+                "data": [
+                    {
+                        "account_name": "suz",
+                        "account_number": "1",
+                        "currency": "ETB",
+                        "amount": 1,
+                        "charge": 0,
+                        "transfer_type": "bank",
+                        "chapa_reference": "7039636706566",
+                        "bank_code": 656,
+                        "bank_name": "Awash Bank",
+                        "bank_reference": null,
+                        "status": "failed/cancelled",
+                        "reference": null,
+                        "created_at": "2022-10-24T14:46:56.000000Z",
+                        "updated_at": "2023-08-07T10:49:59.000000Z"
+                    },
+                    {
+                        "account_name": "suz",
+                        "account_number": "1",
+                        "currency": "ETB",
+                        "amount": 1,
+                        "charge": 0,
+                        "transfer_type": "bank",
+                        "chapa_reference": "7039636674388",
+                        "bank_code": 656,
+                        "bank_name": "Awash Bank",
+                        "bank_reference": null,
+                        "status": "failed/cancelled",
+                        "reference": "7039636674388",
+                        "created_at": "2022-10-24T14:48:06.000000Z",
+                        "updated_at": "2023-10-10T09:20:27.000000Z"
+                    }
+                ]
+            }))
+            .unwrap(),
+        )
+        .create_async()
+        .await;
+
+    let config = ChapaConfigBuilder::new()
+        .base_url(server.url())
+        .api_key("CHASECK-xxxxxxxxxxxxxxxx")
+        .build()
+        .unwrap();
+    let client = ChapaClient::from_config(config).unwrap();
+
+    let response = client
+        .get_all_transfers(
+            Some("2025-01-01"),
+            Some("2025-01-31"),
+            Some("ETB"),
+            Some("success"),
+        )
+        .await
+        .unwrap();
+    assert_eq!(response.status, "success");
+    assert!(!response.message.is_null());
+    assert!(response.data.is_some());
+    mock.assert_async().await;
+}
+#[tokio::test]
+async fn test_bulk_transfers() {}

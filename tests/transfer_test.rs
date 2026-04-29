6b4a36839c3784bb12826c4e6ef14ef88716abb6
abenezer@csa <abenezerhaymanot@gmail.com>
2026-04-29 07:38:43 +0300

    test: add new test swite for transfer endpint

--- tests/transfer_test.rs
+++ tests/transfer_test.rs
@@ -15,39 +15,56 @@ async fn test_get_all_transfers_no_filters() {
         .with_header("content-type", "application/json")
         .with_body(
             serde_json::to_string(&serde_json::json!({
-                "message": "Transfer details fetched",
-                "status": "success",
-                "pagination": {
-                    "current_page": 1,
-                    "first_page_url": "https://api.chapa.co/v1/transfers?page=1",
-                    "last_page": 16,
-                    "last_page_url": "https://api.chapa.co/v1/transfers?page=16",
-                    "next_page_url": "https://api.chapa.co/v1/transfers?page=2",
-                    "path": "https://api.chapa.co/v1/transfers?page=1",
-                    "per_page": 10,
-                    "prev_page_url": null,
-                    "to": 10,
-                    "total": 159,
-                    "error": []
-                },
-                "data": [
-                    {
-                        "account_name": "suz",
-                        "account_number": "1",
-                        "currency": "ETB",
-                        "amount": 1,
-                        "charge": 0,
-                        "transfer_type": "bank",
-                        "chapa_reference": "7039636674388",
-                        "bank_code": 656,
-                        "bank_name": "Awash Bank",
-                        "bank_reference": null,
-                        "status": "failed/cancelled",
-                        "reference": "7039636674388",
-                        "created_at": "2022-10-24T14:48:06.000000Z",
-                        "updated_at": "2023-10-10T09:20:27.000000Z"
-                    }
-                ]
+
+              "message": "Transfer details fetched",
+              "status": "success",
+              "meta": {
+                  "current_page": 1,
+                  "first_page_url": "https://api.chapa.co/v1/transfers?page=1",
+                  "last_page": 16,
+                  "last_page_url": "https://api.chapa.co/v1/transfers?page=16",
+                  "next_page_url": "https://api.chapa.co/v1/transfers?page=2",
+                  "path": "https://api.chapa.co/v1/transfers?page=1",
+                  "per_page": 10,
+                  "prev_page_url": null,
+                  "to": 10,
+                  "total": 159,
+                  "error": []
+              },
+              "data": [
+                  {
+                      "account_name": "suz",
+                      "account_number": "1",
+                      "currency": "ETB",
+                      "amount": 1,
+                      "charge": 0,
+                      "transfer_type": "bank",
+                      "chapa_reference": "7039636706566",
+                      "bank_code": 656,
+                      "bank_name": "Awash Bank",
+                      "bank_reference": null,
+                      "status": "failed/cancelled",
+                      "reference": null,
+                      "created_at": "2022-10-24T14:46:56.000000Z",
+                      "updated_at": "2023-08-07T10:49:59.000000Z"
+                  },
+                  {
+                      "account_name": "Tamiru",
+                      "account_number": "1000089352731",
+                      "currency": "ETB",
+                      "amount": 5,
+                      "charge": 0,
+                      "transfer_type": "bank",
+                      "chapa_reference": "FFY5w8lEYRU",
+                      "bank_code": 893,
+                      "bank_name": "Cooperative Bank of Oromia (COOP)",
+                      "bank_reference": null,
+                      "status": "failed/cancelled",
+                      "reference": "FFY5w8lEYRU",
+                      "created_at": "2023-05-24T13:48:42.000000Z",
+                      "updated_at": "2023-10-10T09:21:27.000000Z"
+                  }
+              ]
             }))
             .unwrap(),
         )
@@ -66,8 +83,8 @@ async fn test_get_all_transfers_no_filters() {
         .await
         .unwrap();
     assert_eq!(response.status, "success");
-    assert!(response.message.is_null());
-    assert!(response.data.is_none());
+    assert_eq!(response.message, "Transfer details fetched");
+    assert!(response.data.is_some());
     mock.assert_async().await;
 }
 
@@ -160,7 +177,7 @@ async fn test_get_all_transfers_with_filters() {
         .await
         .unwrap();
     assert_eq!(response.status, "success");
-    assert!(!response.message.is_null());
+    assert!(!response.message.is_empty());
     assert!(response.data.is_some());
     mock.assert_async().await;
 }
